4118593df8cf05dc3419c36579dea637bb7bca67
AMHM2015 <amhm.05.2011@gmail.com>
2022-10-08 17:54:36 +0300

    insertion sort created

--- src/sorter.java
+++ src/sorter.java
@@ -29,5 +29,19 @@ public class sorter {
             arr[index] = arr[i];  
             arr[i] = smallerNumber;  
         }  
+    }
+    public static void  insertionSort(int[] arr){
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+  
+           
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
     }  
 }
